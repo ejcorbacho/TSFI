6134cd79cccd10212639a62a3781f41aad685892
ejcorbacho <cire.jimenez@gmail.com>
2017-02-09 17:24:30 +0100

    laravel inicial

---
+++ public/js/menu_superior.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  var vez=0;
+  $("#boton_bucador").bind("click", function(){
+
+    $("#componentes_buscador").animate({
+        width: "toggle",
+        opacity: "toggle"
+    });
+    if (vez==0){
+      $("#componentes_buscador").css("display", "block");
+      vez = 1;
+    }
+  });
+
+var menu_mostrado = 0;
+  $("#boton_menu").bind("click", function(){
+    if (menu_mostrado==0){
+      $("#contenido_menu_responsive").fadeIn(500);
+      $("#boton_desplegar_menu").removeClass("fa-bars");
+      $("#boton_desplegar_menu").addClass("fa-times");
+
+      menu_mostrado = 1;
+    } else {
+      $("#contenido_menu_responsive").fadeOut(500);
+      $("#boton_desplegar_menu").addClass("fa-bars");
+      $("#boton_desplegar_menu").removeClass("fa-times");
+      menu_mostrado = 0;
+    }
+  });
+});
---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,191 @@
+<html>
+  <head>
+
+    <title>Inicio </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Oxygen|Poppins" rel="stylesheet">
+    <link href="{{ asset('css/layout.css') }}" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="{{ asset('js/menu_superior.js') }}"></script>
+    <script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
+  </head>
+  <body>
+    <!-- **************************************** MENU GRAN **********************************************-->
+      <div class="container-fluid">
+        <div class="row" id="cabecera_superior"></div>
+        <div class="row">
+          <nav class = "col-md-12 col-sm-12 col-lg-12 hidden-xs" id="menu_superior">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-3 item" id="logotipo">
+                    TSFI
+                </div>
+                <ul class = "col-lg-6 col-md-6 col-sm-6 item" id="items_principales">
+                   <li class="dropdown">
+                      <a href = "#" class="dropdown-toggle" data-toggle = "dropdown">
+                         Java
+                      </a>
+
+                      <div class = "dropdown-menu">
+
+                          <a href="#"><div class="item-submenu">item 1 </div></a>
+                          <a href="#"><div class="item-submenu">item 1 </div></a>
+                          <a href="#"><div class="item-submenu">item 1 </div></a>
+                          <a href="#"><div class="item-submenu">item 1 </div></a>
+                          <a href="#"><div class="item-submenu">item 1 </div></a>
+
+                      </div>
+
+                   </li>
+
+
+
+                </ul>
+
+                <div class="col-lg-3 col-md-3 col-sm-3 item" id="buscador">
+
+                  <div id="componentes_buscador">
+
+                    <ul id="desplegable_buscador"><li class="dropdown">
+                       <a href = "#" class="dropdown-toggle" data-toggle = "dropdown">
+                         CATEGOR.
+                       </a>
+                       <div class = "dropdown-menu">
+                           <a href="#"><div class="item-submenu">item 1 </div></a>
+                           <a href="#"><div class="item-submenu">item 1 </div></a>
+                           <a href="#"><div class="item-submenu">item 1 </div></a>
+                           <a href="#"><div class="item-submenu">item 1 </div></a>
+                           <a href="#"><div class="item-submenu">item 1 </div></a>
+
+                       </div>
+                    </li></ul>
+                    <input type="text" id="caja_buscador" placeholder="Cerca"/>
+                  </div>
+                  <div id="boton_bucador">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+
+
+          </nav>
+        </div>
+      </div>
+    <!-- ************************************* FI MENU GRAN **********************************************-->
+    <!-- **************************************** MENU RESPONSIVE **********************************************-->
+      <div class="container-fluid">
+
+        <div class="row">
+          <nav class = "hidden-md hidden-sm hidden-lg col-xs-12" id="menu_superior">
+            <div class="row">
+                <div class="col-xs-3 item" id="logotipo">
+                  TSFI
+                </div>
+                <ul class = "col-xs-6 item" id="">
+
+                </ul>
+
+                <div class="col-xs-3 item" id="buscador">
+                  <div id="boton_menu">
+                    <i id="boton_desplegar_menu" class="fa fa-bars" aria-hidden="true"></i>
+
+                  </div>
+
+                </div>
+              </div>
+          </nav>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 hidden-md hidden-sm hidden-lg col-xs-12 "id="contenido_menu_responsive">
+            <div id="buscador_responsive">
+              <input type="text" placeholder="Cerca"/>
+              <select>
+                <option> item</option>
+                <option> item</option>
+                <option> item</option>
+                <option> item</option>
+                <option> item</option>
+                <option> item</option>
+                <option> item</option>
+              </select>
+            </div>
+            <div class="items_menu_responsive">
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div class="item_menu_responsive"> item </div>
+              <div style="clear: both"></div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+    <!-- ************************************* FI MENU GRAN **********************************************-->
+    @yield('content')
+
+    <!-- ************************************** FOOTER ***************************************************-->
+    <footer>
+      <div id="contenido_pie">
+        Join our 868,629 subscribers and get access to the latest tools, freebies, product announcements and much more!
+      </div>
+      <hr />
+      <ul id="items_menu_cms">
+        <li> item submenu</li>
+        <li> item submenu</li>
+        <li> item submenu</li>
+
+
+      </ul>
+      <div style="clear: both"></div>
+      <div id="redes_sociales_footer">
+        <i class="fa fa-facebook-official" aria-hidden="true"></i>
+        <i class="fa fa-twitter" aria-hidden="true"></i>
+        <i class="fa fa-envelope-o" aria-hidden="true"></i>
+        <i class="fa fa-rss" aria-hidden="true"></i>
+      </div>
+    </footer>
+  </body>
+</html>

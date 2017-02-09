8a8833e9379ee8b1039d77525a5830235e23c50e
ejcorbacho <cire.jimenez@gmail.com>
2017-02-09 17:24:30 +0100

    laravel inicial

--- config/app.php
+++ config/app.php
@@ -1,4 +1,4 @@
-<?phpqqW
+<?php
 
 return [
 
---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.frontend')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                @foreach($variable as $s)
+                  <p>{{ $s->titulo }}</p>
+                @endforeach
+                
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

c2fa885584dd9ba18fe3b7e24c0d37dc41f73b17
ejcorbacho <cire.jimenez@gmail.com>
2017-02-16 16:17:11 +0100

    autentificacion funcionando

--- app/Http/Controllers/nuevoUsuarioController.php
+++ app/Http/Controllers/nuevoUsuarioController.php
@@ -7,9 +7,16 @@ use App\Entradas;
 
 class nuevoUsuarioController extends Controller
 {
+
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
   public function index() {
 
 
     return view('nuevoUsuario');
   }
+
+
 }

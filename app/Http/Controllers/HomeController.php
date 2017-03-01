f65f21d731b4d4ed2a11c983d53961ce72a0f5ef
josephmob <puki96@hotmail.com>
2017-03-01 17:37:03 +0100

    cambio de nombres de archivos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,6 +27,6 @@ class HomeController extends Controller
     }
     public function editCategory()
     {
-        return view('backendEditCategory');
+        return view('beEditCategory');
     }
 }

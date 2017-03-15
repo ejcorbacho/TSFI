6bfeab0c49dbdb6dcc098cbca7eedfe3fe34eee6
josephmob <puki96@hotmail.com>
2017-03-15 17:16:00 +0100

    organizacion

--- app/Http/Controllers/feController.php
+++ app/Http/Controllers/feController.php
@@ -16,19 +16,19 @@ class feController extends Controller
     private $valor;
 
     public function index() {
-        return view('feHome');
+        return view('frontend.feHome');
     }
     public function category($id) {
         $ocategories = new feCategories;
         $data = $ocategories->llegirCategories($id);
         $post = $ocategories->MostarPosts($id);
 
-        return view('feCategory',['categoria'=>$data[0], 'posts'=>$post]);
+        return view('frontend.feCategory',['categoria'=>$data[0], 'posts'=>$post]);
     }
     public function post($id) {
         $oentradas = new feEntrades;
         $data = $oentradas->llegirEntrada($id);
-        return view('fePost',['data'=>$data[0]]);
+        return view('frontend.fePost',['data'=>$data[0]]);
     }
     //public function category($id) {
     //    $todosposts = new feCateogories;
